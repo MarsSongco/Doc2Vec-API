d54a5140f747353f432f1b0dc4ec286d81d88e78
Mars Frederick <mfsongcowork@outlook.com>
2021-01-26 17:26:18 +0800

    Added ID field to  JSON Response

--- app.py
+++ app.py
@@ -5,6 +5,7 @@
 
 
 import os
+import pandas as pd
 from flask import Flask, request
 from flask_cors import CORS
 from flask_restful import Api, Resource
@@ -17,6 +18,7 @@ app = Flask(__name__) #create the Flask app
 CORS(app)
 
 d2v_model = gensim.models.doc2vec.Doc2Vec.load('doc2vec.model')
+w_ids = pd.read_csv('articles.csv')
 
 @app.route('/getSimilar')
 def getSimilar():
@@ -25,7 +27,14 @@ def getSimilar():
     tokens1 = query.split()
     new_vector = d2v_model.infer_vector(tokens1)
     sims2 = d2v_model.docvecs.most_similar([new_vector])
-    return json.dumps(sims2)
+    results = pd.DataFrame(sims2)
+    results.columns = ['title','cosine score']
+    results = results.merge(w_ids, on = "title", how = 'inner')
+    response = results.to_json(orient="records")
+    parsed = json.loads(response)
+    return json.dumps(parsed)
 if __name__ == '__main__':
     app.run(debug=True)
 
+
+# %%
